409a96827860a34e6565c501471d8dd0c405da5e
Jonathan Samuels <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2024-05-18 13:46:02 -0700

    minor edits

---
+++ IceMan-skeleton-windows/IceMan/IceMan/Actor.h
@@ -0,0 +1,15 @@
+#ifndef ACTOR_H_
+#define ACTOR_H_
+
+#include "GraphObject.h"
+
+// Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
+public:
+    Actor(int imgid,int xpos, int ypos, Direction staDirection, StudentWorld* world, float size = 1.0, unsigned int dept=0 ) alive(true),
+    GraphObject(imageID, startX, startY, startDirection, size, depth), 
+    {
+        setVisible(true);
+    }
+};
+
+#endif // ACTOR_H_
