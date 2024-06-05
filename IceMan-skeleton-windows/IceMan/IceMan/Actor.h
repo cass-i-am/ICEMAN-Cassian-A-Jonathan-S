7b5b19c288d2fef56ac63ab8fd525e35249a3cb2
Jonathan Samuels <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2024-06-05 12:14:26 -0700

    student world start

--- IceMan-skeleton-windows/IceMan/IceMan/Actor.h
+++ IceMan-skeleton-windows/IceMan/IceMan/Actor.h
@@ -134,3 +134,5 @@ public:
 #endif // ACTOR_H_
 
 #endif // ACTOR_H_
+
+
--- IceMan-skeleton-windows/IceMan/IceMan/StudentWorld.h
+++ IceMan-skeleton-windows/IceMan/IceMan/StudentWorld.h
@@ -1,48 +1,3 @@
-#ifndef STUDENTWORLD_H_
-#define STUDENTWORLD_H_
-
-#include "GameWorld.h"
-#include "GameConstants.h"
-#include <string>
-
-// Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
-
-class StudentWorld : public GameWorld
-{
-public:
-	StudentWorld(std::string assetDir)
-		: GameWorld(assetDir)
-	{
-	}
-
-	virtual int init()
-	{
-		return GWSTATUS_CONTINUE_GAME;
-	}
-
-	virtual int move()
-	{
-		// This code is here merely to allow the game to build, run, and terminate after you hit enter a few times.
-		// Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
-		decLives();
-		return GWSTATUS_PLAYER_DIED;
-	}
-
-	virtual void cleanUp()
-	{
-	}
-
-private:
-};
-
-#endif // STUDENTWORLD_H_
-
-
-
-
-
-
-
 // init() Details
 
 // 	Your StudentWorlds init() method must:
@@ -82,6 +37,12 @@ private:
 
 class StudentWorld : public GameWorld
 {
+private:
+
+Iceman* iceman;
+
+Ice* IceField[64][64];
+
 public:
 	StudentWorld(std::string assetDir) : GameWorld(assetDir){}
 
@@ -136,11 +97,7 @@ public:
 		// put code here..... 
 	}
 
-private:
 
-Iceman* iceman;
-
-Ice* IceField[64][64];
 
 };
 
