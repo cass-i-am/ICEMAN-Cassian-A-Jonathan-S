0dbfc32d054e7ef06940f0e11932043880374338
Jonathan Samuels <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2024-06-05 11:37:10 -0700

    reload of system files

---
+++ IceMan-skeleton-windows/IceMan/IceMan/Actor.cpp
@@ -0,0 +1,16 @@
+#include "Actor.h"
+#include "StudentWorld.h"
+
+// Students:  Add code to this file (if you wish), Actor.h, StudentWorld.h, and StudentWorld.cpp
+#include "Actor.h"
+#include "StudentWorld.h"
+
+// Students:  Add code to this file (if you wish), Actor.h, StudentWorld.h, and StudentWorld.cpp
+class Actor : public GraphObject{
+public:
+
+    Actor::Actor(int imageID, int startx, int starty, Direction dir, StudentWorld* world, double size, unsigned int depth): GraohObject(imageID, startX, startY, dir ){
+        setVisibility(true);
+    }
+
+}; 
