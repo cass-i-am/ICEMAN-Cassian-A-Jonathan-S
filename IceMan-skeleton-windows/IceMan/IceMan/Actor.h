4877d336301669ca183ef5d525d1ee92eaf456ca
Jonathan Samuels <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2024-06-05 16:37:18 -0700

    integrating actor.h

--- IceMan-skeleton-windows/IceMan/IceMan/Actor.h
+++ IceMan-skeleton-windows/IceMan/IceMan/Actor.h
@@ -3,45 +3,64 @@
 
 #include "GraphObject.h"
 
+// Forward declaration of StudentWorld
+class StudentWorld;
+
 class Actor : public GraphObject {
 public:
-    Actor(int img_ID, int startX, int startY, Direction dir = right, double size = 1.0, unsigned int depth = 0) 
-        : GraphObject(img_ID, startX, startY, dir, size, depth) {}
+    Actor(int imageID, int startX, int startY, Direction dir, StudentWorld* world, double size = 1.0, unsigned int depth = 0);
+    virtual void doSomething() = 0; // pure virtual function making Actor an abstract class
+    virtual bool isAlive() const { return alive; }
+    virtual void setDead() { alive = false; }
+    virtual ~Actor() = 0; // pure virtual destructor to make the class abstract
 
-    void doSomething() {}
+protected:
+    StudentWorld* getWorld() const { return m_world; }
+
+private:
+    bool alive;
+    StudentWorld* m_world;
 };
 
-class Player : public Actor {
-private:
-    int hp;
-    int sonar;
-    int gold;
+inline Actor::~Actor() {} // Provide a definition for the pure virtual destructor
 
+class Iceman : public Actor {
 public:
-    Player(int img_ID, int startX, int startY, Direction dir = right, double size = 1.0, unsigned int depth = 0) 
-        : Actor(img_ID, startX, startY, dir, size, depth) 
-    {
-        hp = 10;
-        sonar = 1;
-        gold = 0;
-    }
-
-    virtual void doSomething() {}
+    Iceman(int startX, int startY, StudentWorld* world);
+    virtual void doSomething() override;
 };
 
 class Protester : public Actor {
 public:
-    virtual void doSomething() {}
+    Protester(int imageID, int startX, int startY, StudentWorld* world, Direction dir = left, double size = 1.0, unsigned int depth = 0);
+    virtual void doSomething() override = 0; // Protester is abstract
 };
 
-class IceBlock : public Actor {
+class HardcoreProtester : public Protester {
+public: 
+    HardcoreProtester(int startX, int startY, StudentWorld* world);
+    virtual void doSomething() override;
+};
+
+class Ice : public Actor {
 public:
-    IceBlock(int img_ID, int startX, int startY, Direction dir = right, double size = 0.25, unsigned int depth = 3) 
-        : Actor(img_ID, startX, startY, dir, size, depth) {}
-
-    virtual void doSomething() {}
+    Ice(int startX, int startY, StudentWorld* world);
+    virtual void doSomething() override;
 };
 
-// Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
+class GoldNugget : public Actor {
+public:
+    GoldNugget(int startX, int startY, bool isPermanent, StudentWorld* world);
+    virtual void doSomething() override;
+
+private:
+    bool m_isPermanent;
+};
+
+class OilBarrel : public Actor {
+public:
+    OilBarrel(int startX, int startY, StudentWorld* world);
+    virtual void doSomething() override;
+};
 
 #endif // ACTOR_H_
